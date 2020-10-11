a52469c1499f2b0200286cab6c6e0446a6ebeac2
dlrandy <1208484996@qq.com>
2020-10-11 11:57:39 +0800

    feat: init prj

--- src/index.js
+++ src/index.js
@@ -1,17 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import codegen from 'codegen.macro'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// eslint-disable-next-line
+codegen`module.exports = require('@kentcdodds/react-workshop-app/codegen')`
